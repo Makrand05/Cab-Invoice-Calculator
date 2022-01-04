d5075faa5a8fb580285c5e6621cda3fc1b864ea8
Makrand <makrandshingare@gmail.com>
2022-01-04 08:58:31 +0530

    Added : Welcome message

---
+++ src/main/java/com/biz/cabinvice/service/InvoiceService.java
@@ -0,0 +1,7 @@
+package com.biz.cabinvice.service;
+
+public class InvoiceService {
+    public static void main(String[] args) {
+        System.out.println("Welcome in Cab Invoice calculator");
+    }
+}
